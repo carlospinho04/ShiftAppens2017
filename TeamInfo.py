919f73995bc65a8e59d80f27f1a77d1209b86be6
Carlos Pinho <kakasp04@gmail.com>
2017-02-18 17:17:41 +0000

    some improves

--- TeamInfo.py
+++ TeamInfo.py
@@ -1,27 +1,38 @@
 import sys
 import json
 import TwitterApi as ta
+import tests as t
 
 def get_tweets_from_team():
     team_name = sys.argv[1]
-    team_members_twitter = get_teams_members_twitter(team_name)
+    region = sys.argv[2]
+    team_members_twitter = get_teams_members_twitter(team_name, region)
     for player_twitter in team_members_twitter:
         print "PLAYER TWITTER NAME: " + player_twitter
         ta.get_player_emotion(player_twitter)
-def get_teams_members_twitter(team_name):
+
+def get_kda_from_team():
+    team_name = sys.argv[1]
+    region = sys.argv[2]
+    nicks_team = get_team_nicks(team_name, region)
+    for player in nicks_team:
+        t.game_tests(player, region)
+        
+def get_teams_members_twitter(team_name, region):
     players_twitter = []
     with open('teams.json') as team_info:
         data = json.load(team_info)
-    for team in data['games']['eu']['teams']:
+    for team in data['games'][region]['teams']:
         if team['name'].lower() == team_name.lower():
             team_members = team['members']
             break
     for position in team_members:
          players_twitter.append(team_members[position]['twitter'])
+
     return players_twitter
 
 
-def get_team_stats(region,team_name):
+def get_team_nicks(team_name, region):
     players_stats = []
     with open('teams.json') as team_info:
         data = json.load(team_info)
@@ -31,17 +42,12 @@ def get_team_stats(region,team_name):
             break
     for position in team_members:
         players_stats.append(team_members[position]['nick'])
-    if(region.lower()=='lol_na')
-        return  {region:'na',nicks:players_stats} 
-    else
-        return  {region:'euw',nicks:players_stats} 
+
+    return  players_stats 
 
     
 
-
-
 if __name__ == '__main__':
-    #get_tweets_from_team()
-    get_team_stats()
-
+    get_tweets_from_team()
+    #get_kda_from_team()
 
--- tests.py
+++ tests.py
@@ -1,5 +1,3 @@
-# these tests are pretty bad, mostly to make sure no exceptions are thrown
-
 import time
 import json
 import sys
@@ -14,41 +12,23 @@ def wait():
     while not w.can_make_request():
         time.sleep(1)
 
-#def game_tests(username,region):
-def game_tests():
+def game_tests(name, region):
     wait()
-    #region = raw_input('What is your region? (EUROPE_WEST/NORTH_AMERICA)')
-    name = sys.argv[1]
-    print name
-    region = sys.argv[2]
-    print region
-    summoner = w.get_summoner(name=name,region=region)
-    x=w.get_recent_games(summoner['id'],region =region)
-    kills=0
+    summoner = w.get_summoner(name = name, region = region)
+    x = w.get_recent_games(summoner['id'], region = region)
+    kills = 0
     deaths = 0
     assists = 0
     wins = 0
     
-    for i in range(10):
-        deaths +=x['games'][i]['stats']['numDeaths'] if 'numDeaths' in x['games'][i]['stats'] else 0
-        assists +=x['games'][i]['stats']['assists'] if 'assists' in x['games'][i]['stats'] else 0
-        kills += x['games'][i]['stats']['championsKilled'] if 'championsKilled' in x['games'][i]['stats'] else 0
-        if(x['games'][i]['stats']['win'] == True):
-            wins+=1
-    kda = (kills+assists)/(deaths*1.00)
-    wrate =wins/10.0
-    print "WinRate: ", wrate ," KDA: " ,"%.3f" % round(kda,3)
+    for t in x['games']:
+        deaths += t['stats']['numDeaths'] if 'numDeaths' in t['stats'] else 0
+        assists += t['stats']['assists'] if 'assists' in t['stats'] else 0
+        kills += t['stats']['championsKilled'] if 'championsKilled' in t['stats'] else 0
+        if(t['stats']['win']):
+            wins += 1
+    kda = (kills + assists) / (deaths * 1.00)
+    wrate = wins/10.0
+    print "WinRate: ", wrate ," KDA: " ,"%.3f" % round(kda, 3)
     #return json.dumps({'WinRate': wrate ,'KDA':"%.3f" % round(kda,3)})
 
-
-
-def main():
-    region = EUROPE_WEST
-    name = 'PSG Steve'
-    s = w.get_summoner(name=name,region=region)
-    game_tests(s,region)
-
-    #match_tests(m)
-
-if __name__ == '__main__':
-	game_tests()
