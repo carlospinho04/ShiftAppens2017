39fefd6b5af65fee29d962fc7b6e0fa3c2cdf8d5
gbcardoso <gbcardoso96@gmail.com>
2017-02-18 15:43:44 +0000

    name update

--- TeamInfo.py
+++ TeamInfo.py
@@ -12,13 +12,36 @@ def get_teams_members_twitter(team_name):
     players_twitter = []
     with open('teams.json') as team_info:
         data = json.load(team_info)
-    for team in data['games']['lol_eu']['teams']:
+    for team in data['games']['eu']['teams']:
         if team['name'].lower() == team_name.lower():
             team_members = team['members']
+            break
     for position in team_members:
          players_twitter.append(team_members[position]['twitter'])
     return players_twitter
 
+
+def get_team_stats(region,team_name):
+    players_stats = []
+    with open('teams.json') as team_info:
+        data = json.load(team_info)
+    for team in data['games'][region]['teams']:
+        if team['name'].lower() == team_name.lower():
+            team_members = team['members']
+            break
+    for position in team_members:
+        players_stats.append(team_members[position]['nick'])
+    if(region.lower()=='lol_na')
+        return  {region:'na',nicks:players_stats} 
+    else
+        return  {region:'euw',nicks:players_stats} 
+
+    
+
+
+
 if __name__ == '__main__':
-    get_tweets_from_team()
+    #get_tweets_from_team()
+    get_team_stats()
+
 
